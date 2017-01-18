5fcb75d45a3e94406719d12b7f8b697d73e82bcb
Martin Heidegger <martin.heidegger@gmail.com>
2017-01-18 12:17:07 +0900

    Added session support (minor)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var defaults = require('101/defaults')
 var createCrypto = require('./lib/createCrypto.js')
 var rnd = require('./lib/rnd.js')
 var isRedirect = require('./lib/isRedirect.js')
+var pbkdf2Promise = require('./lib/pbkdf2Promise.js')
 
 /**
  * create a graphql-fetch bound to a specific graphql url
@@ -33,14 +34,61 @@ module.exports = function factory (graphqlUrl, keyID, privateKey, cipherAlgorith
     cipherAlgorithm = 'aes256'
   }
   var crypto = createCrypto(cipherAlgorithm, privateKey, cipherPad)
-  var prepareHeaders = function (headers) {
+  var prepareHeaders = function (headers, session) {
     // Override the cipher & keyID & content-transfer-encoding,
     // to make sure that the correct cipher is set
     headers.set('x-cipher', cipherAlgorithm)
-    headers.set('x-key-id', keyID)
+    headers.set('x-key-id', session ? session.keyID : keyID)
     headers.set('content-transfer-encoding', 'base64')
     headers.set('content-type', 'application/egraphql')
   }
+
+  var fetchSession = function fetchSession () {
+    var request = fetch(graphqlUrl, {
+      headers: prepareHeaders(new Headers()),
+      body: crypto.cipher(JSON.stringify({
+        id: factory.id() + '#session',
+        payload: {
+          query: 'session { keyID, privateKey }'
+        }
+      }))
+    }).then(function (res) {
+      var contentType = res.headers.get('content-type')
+      if (res.status === 200 && contentType === 'application/egraphql') {
+        return res.text()
+          .then(function (text) {
+            return JSON.parse(crypto.decipher(text)).payload.session
+          })
+          .then(function (session) {
+            if (!session) {
+              return null
+            }
+            return pbkdf2Promise(privateKey, session.secret, 100000, 256, 'sha512')
+              .then(function (privateKey) {
+                session.privateKey = privateKey
+                return session
+              })
+          })
+      }
+      return null
+    }).catch(function () {
+      // Eat errors
+      return null
+    }).then(function (session) {
+      getSession = function repeatRequestSession () {
+        // Resolve future session requests with the new session
+        return Promise.resolve(session)
+      }
+      return session
+    })
+
+    getSession = function parallelRequestSession () {
+      // Support parallel session requests
+      return request
+    }
+    return request
+  }
+  var getSession = fetchSession
 
   /**
    * graphql fetch - fetch w/ smart defaults for graphql requests
@@ -66,63 +114,62 @@ module.exports = function factory (graphqlUrl, keyID, privateKey, cipherAlgorith
       opts.redirect = 'error'
     }
 
-    prepareHeaders(opts.headers)
-
-    var id
-    if (opts.id) {
-      id = opts.id
-      delete opts.id
-    } else {
-      id = factory.id()
+    if (!opts.id) {
+      opts.id = factory.id()
     }
 
-    opts.body = crypto.cipher(JSON.stringify({
-      id: id,
-      payload: {
-        query: query,
-        variables: variables || {}
-      }
-    }))
+    return getSession()
+      .then(function (session) {
+        prepareHeaders(opts.headers, session)
 
-    return fetch(graphqlUrl, opts)
-      .then(function (res) {
-        var contentType = res.headers.get('content-type')
-        if (res.status === 200 && contentType !== 'application/egraphql') {
-          var err = new Error(
-            'Wrong response content-type: "' + contentType + '". ' +
-            'Is this an egraphql endpoint?'
-          )
-          err.code = 'EHTTPCONTENTTYPE'
-          err.contentType = contentType
-          return Promise.reject(err)
-        }
-        return res.text()
-          .then(function (text) {
-            // fetch will resolve all redirect if the redirect option is not
-            // 'error'. If a redirect is still arriving then the redirect needs
-            // to be handled manually. We shouldn't just "resolve" it because
-            // mean we resolve with data. Thus sending a proper reject error
-            // is clearer
-            if (isRedirect(res.status)) {
-              var redirectErr = new Error('HTTP redirect: ' + res.status + '\n' + text)
-              redirectErr.code = 'EHTTPREDIRECT'
-              redirectErr.status = res.status
-              redirectErr.location = res.headers.get('location')
-              redirectErr.text = text
-              return Promise.reject(redirectErr)
-            }
-            if (res.status !== 200) {
-              var err = new Error('HTTP status: ' + res.status + '\n' + text)
-              err.code = 'EHTTPSTATUS'
-              err.status = res.status
-              err.text = text
+        opts.body = crypto.cipher(JSON.stringify({
+          id: opts.id,
+          payload: {
+            query: query,
+            variables: variables || {}
+          }
+        }), session && session.privateKey)
+
+        return fetch(graphqlUrl, opts)
+          .then(function (res) {
+            var contentType = res.headers.get('content-type')
+            if (res.status === 200 && contentType !== 'application/egraphql') {
+              var err = new Error(
+                'Wrong response content-type: "' + contentType + '". ' +
+                'Is this an egraphql endpoint?'
+              )
+              err.code = 'EHTTPCONTENTTYPE'
+              err.contentType = contentType
               return Promise.reject(err)
             }
-            return crypto.decipher(text)
+            return res.text()
+              .then(function (text) {
+                // fetch will resolve all redirect if the redirect option is not
+                // 'error'. If a redirect is still arriving then the redirect needs
+                // to be handled manually. We shouldn't just "resolve" it because
+                // mean we resolve with data. Thus sending a proper reject error
+                // is clearer
+                if (isRedirect(res.status)) {
+                  var redirectErr = new Error('HTTP redirect: ' + res.status + '\n' + text)
+                  redirectErr.code = 'EHTTPREDIRECT'
+                  redirectErr.status = res.status
+                  redirectErr.location = res.headers.get('location')
+                  redirectErr.text = text
+                  return Promise.reject(redirectErr)
+                }
+                if (res.status !== 200) {
+                  var err = new Error('HTTP status: ' + res.status + '\n' + text)
+                  err.code = 'EHTTPSTATUS'
+                  err.status = res.status
+                  err.text = text
+                  return Promise.reject(err)
+                }
+                return crypto.decipher(text, session && session.privateKey)
+              })
+              .then(function (data) {
+                return JSON.parse(data).payload
+              })
           })
-      })
-      .then(function (data) {
-        return JSON.parse(data).payload
       })
   }
 }
--- test/graphql-fetch.unit.js
+++ test/graphql-fetch.unit.js
@@ -1,6 +1,6 @@
 'use strict'
 
-/* global Headers */
+/* global Headers, Response */
 require('isomorphic-fetch')
 
 var test = require('blue-tape').test
@@ -13,8 +13,8 @@ var graphqlUrl = 'http://host/graphql'
 var graphqlFactory = require('../index.js')
 var toBuffer = require('../lib/toBuffer.js')
 
-function _encrypt (cipher, key, pad, data) {
-  var c = crypto.createCipher(cipher, key)
+function _encrypt (cipher, key, pad, data, sessionPrivateKey) {
+  var c = crypto.createCipher(cipher, sessionPrivateKey || key)
   data = JSON.stringify(data)
   var missing = pad - (data.length % pad)
   for (var i = 0; i < missing; i++) {
@@ -26,15 +26,15 @@ function _encrypt (cipher, key, pad, data) {
   ]).toString('base64')
 }
 
-function _decrypt (cipher, key, data) {
-  var d = crypto.createDecipher(cipher, key)
-  return Buffer.concat([
+function _decrypt (cipher, key, data, sessionPrivateKey) {
+  var d = crypto.createDecipher(cipher, sessionPrivateKey || key)
+  return JSON.parse(Buffer.concat([
     d.update(toBuffer(data, 'base64')),
     d.final()
-  ]).toString()
+  ]).toString())
 }
 
-function testRequest (t, request, response) {
+function testRequest (t, request, response, handleRequest) {
   var encrypt = _encrypt.bind(null, request.cipherAlgorithm || 'aes256', request.privateKey, request.cipherPad || 1024)
   var decrypt = _decrypt.bind(null, request.cipherAlgorithm || 'aes256', request.privateKey)
   var res = {
@@ -44,55 +44,80 @@ function testRequest (t, request, response) {
     }),
     text: sinon.stub().returns(Promise.resolve(encrypt({
       payload: response.payload
-    })))
+    }, request.sessionPrivateKey)))
   }
-  sinon.stub(global, 'fetch').resolves(res)
+  var fetchStub = sinon.stub(global, 'fetch').resolves(res)
+  global.fetch = function (url, opts) {
+    var req = decrypt(opts.body)
+    t.notEqual(req, null, 'req != null')
+    if (request.handleSession) {
+      global.fetch = fetchStub
+      return request.handleSession(url, encrypt, decrypt)
+    }
+    t.deepEqual(req.payload.query, 'session { keyID, privateKey }')
+    if (opts.headers) {
+      t.equal(opts.headers.get('x-key-id'), request.keyID)
+    }
+    t.ok(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{1,3}Z_[A-Za-z0-9]{16}#session$/g.test(req.id))
+    var res = new Response(encrypt({
+      payload: {
+        session: request.session
+      }
+    }))
+    res.headers.set('content-type', 'application/egraphql')
+    global.fetch = fetchStub
+    return Promise.resolve(res)
+  }
+
   try {
     var graphqlFetch = graphqlFactory(request.url, request.keyID, request.privateKey, request.cipherAlgorithm, request.cipherPad)
   } catch (e) {
-    global.fetch.restore()
+    fetchStub.restore()
     return Promise.reject(e)
   }
-  return graphqlFetch(request.query, request.variables, request.opts)
-    .then(function (result) {
-      t.same(result, response.payload, 'result be response payload')
-      sinon.assert.calledOnce(global.fetch)
-      sinon.assert.calledOnce(res.text)
-      sinon.assert.calledWith(global.fetch, request.url, sinon.match(function (opts) {
-        var json = JSON.parse(decrypt(opts.body))
-        opts.json = json
-        t.deepEqual(Object.keys(json), ['id', 'payload'], 'Object.keys(json)')
-        t.ok(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{1,3}Z_[A-Za-z0-9]{16}$/g.test(json.id), 'id pattern')
-        t.deepEqual(json.payload, {
-          query: request.query,
-          variables: request.variables || {}
-        }, 'payload comparison')
-        t.equal(opts.method, 'POST', 'opts.method')
-        t.equal(opts.redirect, request.redirect || 'error', 'opts.redirect')
-        t.ok(opts.headers instanceof Headers, 'opts.headers instanceof Headers')
-        t.equal(opts.headers.get('x-cipher'), request.cipherAlgorithm || 'aes256', 'opts.headers.get("x-cipher")')
-        t.equal(opts.headers.get('x-key-id'), request.keyID, 'opts.headers.get("x-key-id")')
-        t.equal(opts.headers.get('content-transfer-encoding'), 'base64', 'opts.headers.get("content-transfer-encoding")')
-        t.equal(opts.headers.get('content-type'), 'application/egraphql', 'opts.headers.get("content-type")')
-        if (request.checkOpts) {
-          return request.checkOpts(opts)
+
+  var doRequest = function () {
+    return graphqlFetch(request.query, request.variables, request.opts)
+      .then(function (result) {
+        t.same(result, response.payload, 'result be response payload')
+        sinon.assert.calledWith(global.fetch, request.url, sinon.match(function (opts) {
+          var json = decrypt(opts.body, request.sessionPrivateKey)
+          opts.json = json
+          t.deepEqual(Object.keys(json), ['id', 'payload'], 'Object.keys(json)')
+          t.ok(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{1,3}Z_[A-Za-z0-9]{16}$/g.test(json.id), 'id pattern')
+          t.deepEqual(json.payload, {
+            query: request.query,
+            variables: request.variables || {}
+          }, 'payload comparison')
+          t.equal(opts.method, 'POST', 'opts.method')
+          t.equal(opts.redirect, request.redirect || 'error', 'opts.redirect')
+          t.ok(opts.headers instanceof Headers, 'opts.headers instanceof Headers')
+          t.equal(opts.headers.get('x-cipher'), request.cipherAlgorithm || 'aes256', 'opts.headers.get("x-cipher")')
+          t.equal(opts.headers.get('x-key-id'), request.session ? request.session.keyID : request.keyID, 'opts.headers.get("x-key-id")')
+          t.equal(opts.headers.get('content-transfer-encoding'), 'base64', 'opts.headers.get("content-transfer-encoding")')
+          t.equal(opts.headers.get('content-type'), 'application/egraphql', 'opts.headers.get("content-type")')
+          if (request.checkOpts) {
+            return request.checkOpts(opts)
+          }
+          return true
+        }))
+        return result
+      })
+      .then(function (result) {
+        return {
+          result: result,
+          encrypt: encrypt
         }
-        return true
-      }))
-      return result
-    })
-    .then(function (result) {
-      return {
-        result: result,
-        encrypt: encrypt
-      }
-    })
+      })
+  }
+  var req = (handleRequest ? handleRequest(doRequest) : doRequest())
+  return req
     .catch(function (e) {
-      global.fetch.restore()
+      fetchStub.restore()
       return Promise.reject(e)
     })
     .then(function () {
-      global.fetch.restore()
+      fetchStub.restore()
     })
 }
 
@@ -147,6 +172,83 @@ test('make a graphql request', function (t) {
     payload: {
       data: 'something'
     }
+  })
+})
+
+test('make a sessioned, graphql request', function (t) {
+  var secret = crypto.randomBytes(256).toString('base64')
+  var pass = 'password'
+  return testRequest(t, {
+    url: graphqlUrl,
+    keyID: 'admin',
+    cipherAlgorithm: 'aes-256-cbc',
+    privateKey: pass,
+    sessionPrivateKey: crypto.pbkdf2Sync(pass, secret, 100000, 256, 'sha512'),
+    session: {
+      keyID: 'other',
+      secret: secret
+    },
+    query: 'query { user { username } }'
+  }, {
+    payload: {
+      data: 'something'
+    }
+  })
+})
+
+test('handle errors in session management', function (t) {
+  var pass = 'password'
+  return testRequest(t, {
+    url: graphqlUrl,
+    keyID: 'admin',
+    cipherAlgorithm: 'aes-256-cbc',
+    privateKey: pass,
+    handleSession: function () {
+      return Promise.reject(new Error('just some error'))
+    },
+    query: 'query { user { username } }'
+  }, {
+    payload: {
+      data: 'something'
+    }
+  }, function handleRequest (doRequest) {
+    return Promise.all([
+      doRequest(),
+      doRequest()
+    ]).then(function () {
+      return doRequest()
+    })
+  })
+})
+
+test('handle null result in session management', function (t) {
+  var pass = 'password'
+  return testRequest(t, {
+    url: graphqlUrl,
+    keyID: 'admin',
+    cipherAlgorithm: 'aes-256-cbc',
+    privateKey: pass,
+    handleSession: function (url, encrypt) {
+      var res = new Response(encrypt({
+        payload: {
+          session: null
+        }
+      }))
+      res.headers.set('content-type', 'application/egraphql')
+      return Promise.resolve(res)
+    },
+    query: 'query { user { username } }'
+  }, {
+    payload: {
+      data: 'something'
+    }
+  }, function handleRequest (doRequest) {
+    return Promise.all([
+      doRequest(),
+      doRequest()
+    ]).then(function () {
+      return doRequest()
+    })
   })
 })
 
